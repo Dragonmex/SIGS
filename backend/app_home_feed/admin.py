f82dde4efc97ff5d954280d70cb0caf402daa149
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-11-02 00:07:40 -0300

    Update admin.py

--- backend/app_home_feed/admin.py
+++ backend/app_home_feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Banner, Categoria, Funcionalidade, LinkRapido, Noticia, Video
 
-# Register your models here.
+# Registrando cada modelo para aparecer na interface do admin
+admin.site.register(Banner)
+admin.site.register(Categoria)
+admin.site.register(Funcionalidade)
+admin.site.register(LinkRapido)
+admin.site.register(Noticia)
+admin.site.register(Video)
