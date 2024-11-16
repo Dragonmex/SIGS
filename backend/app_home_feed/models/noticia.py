62526091f55bba233b6b4137511d8f38e33905b4
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-11-15 23:27:20 -0300

    Update model noticia

--- backend/app_home_feed/models/noticia.py
+++ backend/app_home_feed/models/noticia.py
@@ -1,12 +1,12 @@
 from django.db import models
-from .categoria import Categoria 
+from .categoria import Categoria
 
 class Noticia(models.Model):
     titulo = models.CharField(max_length=255)
     conteudo = models.TextField()
     imagem = models.ImageField(upload_to='noticias_imagens/', blank=True, null=True)
     categoria = models.ForeignKey(Categoria, related_name='noticias', on_delete=models.CASCADE)
-    noticias_relacionadas = models.ManyToManyField('self', blank=True)
+    noticias_relacionadas = models.ManyToManyField('self', blank=True, related_name='relacionadas')
     data_publicacao = models.DateField(auto_now_add=True)
 
     def __str__(self):
