830680b8fcf977faef7b627f494cf8ee5be30062
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-11-26 02:19:12 -0300

    atualizando urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Rota para o painel de administração
-    path('home/', include('app_optimus.urls')),  # Inclua as rotas da API principal
-    path('', RedirectView.as_view(url='/home/', permanent=True)),  # Redireciona para /home
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='login/', permanent=False)),  # Redireciona a raiz para o login
+    path('', include('app_optimus.urls')),  # Inclui as rotas do app
 ]
--- backend/app_optimus/urls.py
+++ backend/app_optimus/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
-
+from app_optimus.views import login_view, cadastro_view, home_view
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
+    path('login/', login_view, name='login'),  # Página de login
+    path('cadastro/', cadastro_view, name='cadastro_usuario'),  # Página de cadastro
+    path('home/', home_view, name='home'),  # Página inicial após login
 ]
