f7cd0b86d0551fda1a8aba4afa67c9850c087321
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-11-29 00:46:45 -0300

    Update urls.py

--- backend/app_optimus/urls.py
+++ backend/app_optimus/urls.py
@@ -4,14 +4,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
+    # AUTENTICAÇÃO DE USUARIOS
     path('api/login/', LoginAPI.as_view(), name='api-login'),
     path('api/logout/', LogoutAPI.as_view(), name='api-logout'),
     path('api/home/', HomeAPI.as_view(), name='api-home'),
     path('api/register/', CadastroUsuarioAPI.as_view(), name='api-cadastro'),
-    path('api/noticia/<int:noticia_id>/', NoticiaDetailView.as_view(), name='api-noticia-detail'),
-    
-    # Obter Token
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # VISUALIZAÇÃO EM DETALHES - NOTICIAS
+    path('api/noticia/<int:noticia_id>/', NoticiaDetailView.as_view(), name='api-noticia-detail'),
 
 ]
